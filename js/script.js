7f3bdf31b861ed193c22ce24c2e9c693c5c0fe66
Washington Luiz <wlfranca.luiz@hotmail.com>
2022-04-25 21:11:53 -0300

    trabalhe conosco

--- js/script.js
+++ js/script.js
@@ -114,7 +114,10 @@ $(window).scroll(function(e) {
 setTimeout(function() {
 
     $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+    $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
 
 }, 250);
 
 });
+
+
